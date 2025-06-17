ff0f2777a94cdd0b2c49833e180714f58ce473af
Iván Cabrerizo <98966078+IvanCabrerizo@users.noreply.github.com>
2025-06-17 13:38:39 +0200

    App name change from CRT to CPT

--- app/src/main/java/es/mobiledev/cpt/MainActivity.kt
+++ app/src/main/java/es/mobiledev/cpt/MainActivity.kt
@@ -1,4 +1,4 @@
-package es.mobiledev.crt
+package es.mobiledev.cpt
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import es.mobiledev.crt.ui.theme.CRTTheme
+import es.mobiledev.cpt.ui.theme.CPTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CRTTheme {
+            CPTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CRTTheme {
+    CPTTheme {
         Greeting("Android")
     }
 }
