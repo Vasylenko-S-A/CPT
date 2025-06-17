bb04a9ed4ff73dfe31f2d1dcbb438225004da48b
Iván Cabrerizo <98966078+IvanCabrerizo@users.noreply.github.com>
2025-06-17 13:38:39 +0200

    App name change from CRT to CPT

--- app/src/main/java/es/mobiledev/cpt/ui/theme/Color.kt
+++ app/src/main/java/es/mobiledev/cpt/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package es.mobiledev.crt.ui.theme
+package es.mobiledev.cpt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/es/mobiledev/cpt/ui/theme/Type.kt
+++ app/src/main/java/es/mobiledev/cpt/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package es.mobiledev.crt.ui.theme
+package es.mobiledev.cpt.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
